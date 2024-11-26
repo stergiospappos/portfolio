6bddda797f2c8ebd672c093c2b35c4001d14aebb
“Stergios <contact@stergiospappos.me>
2024-11-26 15:29:17 +0400

    redirects

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { BrowserRouter as Router } from "react-router-dom";
 import App from "./App.jsx";
 import "./index.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <Router>
-    <Routes>
-      <Route path="/*" element={<App />} />
-    </Routes>
-  </Router>
+  <StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </StrictMode>
 );
